1b8cc558b01051be14eb1873c066bc2784c18fcd
butter <radradio234@gmail.com>
2022-11-01 23:01:23 -0700

    add other button functionality

--- src/main/java/com/butterutil/mixin/MixinTitleScreenMenu.java
+++ src/main/java/com/butterutil/mixin/MixinTitleScreenMenu.java
@@ -1,8 +1,12 @@
 package com.butterutil.mixin;
 
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.*;
+import net.minecraft.realms.RealmsBridge;
+import net.minecraft.world.demo.DemoWorldServer;
+import net.minecraft.world.storage.ISaveFormat;
+import net.minecraft.world.storage.WorldInfo;
+import net.minecraftforge.fml.client.GuiModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,7 +21,7 @@ import java.io.IOException;
 public abstract class MixinTitleScreenMenu extends GuiScreen {
     @Inject(method = "initGui", at = @At("RETURN"))
     private void initGui(final CallbackInfo cbi) {
-        this.buttonList.add(new GuiButton(69420, this.width / 2, this.height / 2 + 100, "Play Hypixel"));
+        this.buttonList.add(new GuiButton(69420, this.width + 1, this.height / 2 + 100, "Play Hypixel"));
 
         super.initGui();
     }
@@ -25,13 +29,54 @@ public abstract class MixinTitleScreenMenu extends GuiScreen {
     @Override
     protected void actionPerformed(GuiButton button) throws IOException {
         switch (button.id) {
+            //me
             case 69420:
                 System.out.println("works!");
                 break;
+            /**
+             * @author mojang
+             */
+            case 0:
+                this.mc.displayGuiScreen(new GuiOptions(this, this.mc.gameSettings));
+                break;
+            case 1:
+                this.mc.displayGuiScreen(new GuiSelectWorld(this));
+                break;
+            case 2:
+                this.mc.displayGuiScreen(new GuiMultiplayer(this));
+                break;
+            case 4:
+                this.mc.shutdown();
+                break;
+            case 5:
+                this.mc.displayGuiScreen(new GuiLanguage(this, this.mc.gameSettings, this.mc.getLanguageManager()));
+                break;
+            case 6:
+                this.mc.displayGuiScreen(new GuiModList(this));
+                break;
+            case 11:
+                this.mc.launchIntegratedServer("Demo_World", "Demo_World", DemoWorldServer.demoWorldSettings);
+                break;
+            case 12:
+                ISaveFormat isaveformat = this.mc.getSaveLoader();
+                WorldInfo worldinfo = isaveformat.getWorldInfo("Demo_World");
+                if (worldinfo != null) {
+                    GuiYesNo guiyesno = GuiSelectWorld.makeDeleteWorldYesNo(this, worldinfo.getWorldName(), 12);
+                    this.mc.displayGuiScreen(guiyesno);
+                }
+                break;
+            case 14:
+                this.switchToRealms();
+                break;
             default:
-                System.out.println(button.id);
+                System.out.println(button.id + " " + button.displayString);
                 break;
         }
         super.actionPerformed(button);
+    }
+
+    private void switchToRealms() {
+        RealmsBridge realmsbridge = new RealmsBridge();
+        realmsbridge.switchToRealms(this);
     }
 }
